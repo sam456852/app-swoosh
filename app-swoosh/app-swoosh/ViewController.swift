9f8bcfe22524d4e4202cb5275c1f1a1af7af64e2
sam456852 <weixiangzhang@u.northwestern.edu>
2017-10-12 01:24:53 -0500

    initial

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Weixiang Zhang on 10/8/17.
+//  Copyright © 2017 Weixiang Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
