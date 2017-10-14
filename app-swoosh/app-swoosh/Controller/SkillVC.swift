c16110c5ffa6271216f74d33fa54d67ed5bfd4bc
sam456852 <weixiangzhang@u.northwestern.edu>
2017-10-14 01:34:18 -0500

    Added prepare

--- app-swoosh/app-swoosh/Controller/SkillVC.swift
+++ app-swoosh/app-swoosh/Controller/SkillVC.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class SkillVC: UIViewController {
+    var player: Player!
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Weixiang Zhang on 10/14/17.
+//  Copyright © 2017 Weixiang Zhang. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
